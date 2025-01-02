57101d0ba91021f711fdc0d59bb824c1aca94b20
Sathiya3012 <sathiya@tecofize.com>
2025-01-02 12:47:05 +0530

    update

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,4 @@
 export default {
   testEnvironment: "node",
-  transform: {
-    "^.+\\.js$": "babel-jest",
-  },
-  testMatch: ["**/__tests__/**/*.js", "**/?(*.)+(test).js"],
-  moduleFileExtensions: ["js", "json", "node"],
   forceExit: true,
-  verbose: true,
 };
